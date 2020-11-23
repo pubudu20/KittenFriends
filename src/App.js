106922ad9b1cf3ab8b145b839474e0fd2a2b6359
pubudu <pubudukaviranga20@gmail.com>
2020-11-23 16:58:13 +0530

    Add Card components

--- src/App.js
+++ src/App.js
@@ -1,89 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
- 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import CardList from './CardList';
+import {robots} from './robots';
+import SearchBox from './SearchBox';
+
+const App = () => {
+    return(
+        <div>
+            
+        <h1>RoboFriends</h1>
+        <SearchBox/>
+        <CardList robots ={robots}/>
+        </div>
+        );
 }
- 
+
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+    <App/>
+   
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
